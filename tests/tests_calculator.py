ebf2ba90ccfc0b804f25bcb11d78f8b20db9026f
Yafté M. <redyafte@gmail.com>
2025-04-25 14:56:13 -0600

    First commit

---
+++ tests/tests_calculator.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.calculator import sum, subtract, multiply, divide
+
+
+class CalculatorTest(unittest.TestCase):
+
+    def test_sum(self):
+        assert sum(2, 3) == 5
+
+    def test_subtract(self):
+        assert subtract(10, 5) == 5
+
+    def test_multiply(self):
+        assert multiply(3, 2) == 6
+
+    def test_divide(self):
+        assert divide(10, 2) == 5
